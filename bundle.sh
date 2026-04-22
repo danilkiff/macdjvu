cd5006168dc0d807978e53594cd4200e20624f28
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2026-04-23 02:18:39 +0300

    refactor: extract Info.plist from bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -12,34 +12,6 @@ rm -rf "$APP"
 mkdir -p "$MACOS"
 
 cp .build/release/MacDjVu "$MACOS/"
-
-cat > "$CONTENTS/Info.plist" << 'PLIST'
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>CFBundleIdentifier</key>
-    <string>com.macdjvu.app</string>
-    <key>CFBundleName</key>
-    <string>MacDjVu</string>
-    <key>CFBundleDisplayName</key>
-    <string>MacDjVu</string>
-    <key>CFBundleExecutable</key>
-    <string>MacDjVu</string>
-    <key>CFBundlePackageType</key>
-    <string>APPL</string>
-    <key>CFBundleVersion</key>
-    <string>1</string>
-    <key>CFBundleShortVersionString</key>
-    <string>0.1.0</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>NSHighResolutionCapable</key>
-    <true/>
-    <key>LSMinimumSystemVersion</key>
-    <string>15.0</string>
-</dict>
-</plist>
-PLIST
+cp Info.plist "$CONTENTS/Info.plist"
 
 echo "Built: $APP"
